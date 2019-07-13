531e8d023a56a7c2654954ff2bf6248df41af0c9
Aditya Telange <21258296+AdityaTelange@users.noreply.github.com>
2019-07-13 15:59:42 +0530

    Update start.sh

--- bluetooth-player/start.sh
+++ bluetooth-player/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -z "$BLUETOOTH_DEVICE_NAME" ]]; then
-  BLUETOOTH_DEVICE_NAME=$(printf "balenaSound %s" $(hostname | cut -c -4))
+  BLUETOOTH_DEVICE_NAME=$(printf "rpi-bluetooth-%s" $(hostname | cut -c -4))
 fi
 
 # Set the discoverable timeout here
