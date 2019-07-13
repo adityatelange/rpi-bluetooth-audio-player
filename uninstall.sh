5ab6d082b55c8dfd2b6f0f765634dde44ccdc617
Aditya Telange <adityatelange@gmail.com>
2019-07-13 15:52:37 +0530

    init

---
+++ uninstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+else
+	echo "removing copied files"
+
+	rm -rf /usr/src/sounds/
+
+	rm -rf /usr/src/bluetooth-udev
+	rm -rf /etc/udev/rules.d/udev-rules/
+	rm -rf /usr/src/bluetooth-agent
+	rm -rf /usr/src/start.sh
+	rm -rf /usr/local/bin/start_bt_player
+
+	systemctl stop bluetooth-player.service
+	systemctl disable bluetooth-player.service
+	rm -rf /etc/systemd/system/bluetooth-player.service
+fi
